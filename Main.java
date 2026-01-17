25294f8b039668668c81b83d3c3f267724411c8d
rjrommeljanohan <rjrommel.janohan@cvsu.edu.ph>
2026-01-17 20:52:13 +0800

    Implement main class to launch application

---
+++ Main.java
@@ -0,0 +1,13 @@
+import controller.TaskManager;
+import ui.MainWindow;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        TaskManager manager = new TaskManager();
+
+        MainWindow window = new MainWindow(manager);
+        window.setVisible(true);
+    }
+}
